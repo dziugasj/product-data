fab0be655ebba5333bf93dada6bb4e5f4ca2bc4e
Dziugas Juozapaitis <dziugasj@gmail.com>
2023-03-19 18:07:06 +0200

    It is not the best idea to use the Name as a key, but there is no proper identifier.

--- src/main/kotlin/com/example/demo/service/ProductCache.kt
+++ src/main/kotlin/com/example/demo/service/ProductCache.kt
@@ -1,6 +1,7 @@
 package com.example.demo.service
 
 import com.example.demo.dto.Product
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.concurrent.ConcurrentHashMap
 
@@ -9,16 +10,19 @@ class ProductCache {
 
     private val map: MutableMap<String, Product> = ConcurrentHashMap()
 
-
-
-    fun saveProducts(products: List<Product>) {
-
-
-
-//        products.forEach{p -> map.put(p.name, p)}
-
-
+    companion object {
+        val logger = LoggerFactory.getLogger(ProductCache::class.java)!!
     }
 
+    fun saveProducts(products: List<Product>) {
+        products.forEach(::addProductIfPossible)
+    }
 
+    private fun addProductIfPossible(product: Product) {
+        if (product.name != null) {
+            map[product.name] = product
+        } else {
+            logger.warn("Product will be not added to cache since it has NULL name [product=$product]")
+        }
+    }
 }
